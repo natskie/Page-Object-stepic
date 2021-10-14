50e7302569c8d3cfcc5b657f44caf42361732f7b
digitalscreens <a.ornatskyi@digitalscreens.net.ua>
2021-10-14 11:51:46 +0300

    correct login url check

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from locators import LoginPageLocators
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self): 
-        assert "login"in self.browser.current_url(), "Login page is not corrected"
+        assert self.browser.current_url.find("login")>-1 , "Login page is not corrected"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
